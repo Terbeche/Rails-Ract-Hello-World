25c3b9fb5051d223d1f357e4c19d4244208836ab
terbeche <termus96@gmail.com>
2022-10-17 23:27:30 +0100

    Create an API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :messages, only: [:index]
+    end
+  end
+  
   root 'root#index'
 
 end
